d2e62191c0a45fe8968b9b9c17e975d802bfd3f8
alberto.2.garcia <alberto.2.garcia@atos.net>
2022-03-14 17:01:17 -0600

    Added all routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,25 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Navigate, Route, Routes } from 'react-router-dom';
+import HomePage from './container/HomePage';
+import { ROUTE } from './constants/route';
+import CheckoutPage from './container/CheckoutPage';
+import AllProductsPage from './container/AllProductsPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path={ROUTE.ALL_PRODUCTS}  element={<AllProductsPage />}/>
+        <Route path={ROUTE.CHECKOUT}      element={<CheckoutPage />}/>
+        <Route path={ROUTE.HOME}          element={<HomePage />}/>
+        <Route
+          path="*"
+          element={<Navigate to="/" />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
